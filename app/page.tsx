2f4e0ad735105eb8932fb517f258d4ff667ffedd
wolfgunblood <jalajdorai100@gmail.com>
2024-01-18 18:13:38 +0530

    Initialize app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-red-400'>Hello World!!!</h1>
+      <Button>Click ME!!!</Button>
+    </div>
+  )
+}
